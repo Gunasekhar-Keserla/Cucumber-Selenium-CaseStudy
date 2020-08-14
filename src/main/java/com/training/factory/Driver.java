4a0be78fc5c1e612601c74207feb3f4ab10ff612
Keserla, Gunasekhar (gk4161) <gk4161@att.com>
2020-08-14 17:10:27 +0530

    cucumber-selenium-casestudy project

--- src/main/java/com/training/factory/Driver.java
+++ src/main/java/com/training/factory/Driver.java
@@ -5,7 +5,8 @@ package com.training.factory;
 
 public interface Driver {
 	String CHROME_KEY="webdriver.chrome.driver"; 
-	String CHROME_PATH="C:\\Users\\GunasekharKeserla\\Downloads\\chromedriver_win32 (3)\\chromedriver.exe"; 
+	//String CHROME_PATH="C:\\Users\\GunasekharKeserla\\Downloads\\chromedriver_win32 (3)\\chromedriver.exe"; 
+	String CHROME_PATH="C:\\Guna\\workspace\\eclipse-workspace\\ASEoD-mS-Robot\\selenium-cucumber-project3-13-08\\chromedriver.exe\\";
 	
 	String FIREFOX_KEY="webdriver.gecko.driver"; 
 	String FIREFOX_PATH="";  
